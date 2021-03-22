6d3fcc14612bff0f5f93492c1a9743a59bb58b2d
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-22 14:28:22 -0600

    Added files for Game, Players, Gestures. Created objects for each class

---
+++ Game.py
@@ -0,0 +1,5 @@
+class Players:
+    def __init__(self, name):
+        self.name = name
+
+
---
+++ Players.py
@@ -0,0 +1,11 @@
+from Game import Players
+
+
+class Participants:
+    def __init__(self, name):
+        self.name = ""
+        super().__init__()
+
+
+my_participants = Participants("Human")
+my_participants1 = Participants("Computer")
