97bdfad1237ffb871c100b95bcd6a8af16d48a49
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-22 11:42:23 -0600

    Added welcome verbiage and rules

---
+++ rpsls.py
@@ -0,0 +1,19 @@
+import random
+
+
+print("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
+
+x = input('Please enter your name:')
+    print('Hello, ' + x 'Here are the rules:')
+
+print("Rock crushes Scissors")
+print("Scissors cuts Paper")
+print("Paper covers Rock")
+print("Rock crushes Lizard")
+print("Lizard poisons Spock")
+print("Spock smashes Scissors")
+print("Scissors decapitates Lizard")
+print("Lizard eats Paper")
+print("Paper disproves Spock")
+print("Spock vaporizes Rock")
+
