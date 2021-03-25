1427281e34502872241836e5842cb10eba26eb33
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-24 21:55:17 -0600

    Final commit, added playertwo for 2ns human. tested score board

--- rpsls.py
+++ rpsls.py
@@ -1,7 +1,7 @@
-from random import randint
+import random
 from Game import Game, my_rules, my_gestures
 from Computer import Computer, computer
-
+from unittest import result
 
 x = input('Please enter your name:')
 print('Hello, ' + x + '. Good luck!')
@@ -13,42 +13,68 @@ print()
 print('The best of 3 will win the game!')
 print()
 
-computer_gesture = my_gestures[randint(0, 4)]
-playerOne = False
-
-while playerOne == False:
-    playerOne = input("Please enter your gesture:")
-print("Computer Chooses " + my_gestures[randint(0, 4)])
-if playerOne == computer_gesture:
-    print("Tie!")
-elif playerOne == "rock":
-    if computer_gesture == "paper" or "spock":
-        print("Computer Wins!")
-    else:
-        print(x + "Wins!")
-elif playerOne == "paper":
-    if computer_gesture == "lizard" or "scissors":
-        print("Computer Wins!")
-    else:
-        print(x + "Wins!")
-elif playerOne == "scissors":
-    if computer_gesture == "rock" or "spock":
-        print("Computer Wins!")
-    else:
-        print(x + "Wins!")
-elif playerOne == "lizard":
-    if computer_gesture == "rock" or "scissors":
-        print("Computer Wins!")
-    else:
-        print(x + "Wins!")
-elif playerOne == "Spock":
-    if computer_gesture == "paper" or "lizard":
-        print("Computer Wins!")
-    else:
-        print(x + "Wins!")
-else:
-    print("Please enter a valid gesture!")
-
 playerOne_score = int(0)
 computer_score = int(0)
 score_limit = 5
+while playerOne_score != score_limit or computer_score != score_limit:
+    playerOne: str = input(str("Please enter your gesture:")).lower()
+
+    computer_move = random.choice(my_gestures)
+
+    print("The computer chooses", computer_move)
+
+    if computer_move == "rock" and playerOne == "rock":
+        print("Tie!!")
+
+    if computer_move == "paper" and playerOne == "paper":
+        print("Tie!!")
+
+    if computer_move == "scissors" and playerOne == "scissors":
+        print("Tie!!")
+
+    if computer_move == "lizard" and playerOne == "lizard":
+        print("Tie!!")
+
+    if computer_move == "Spock" and playerOne == "Spock":
+        print("Tie!!")
+
+    elif computer_move == "paper" and playerOne == "rock" or "Spock":
+        print("The computer scores")
+
+        computer_score = computer_score + 1
+        print("The computers score is:", computer_score)
+
+    elif computer_move == "rock" and playerOne == "paper" or "Spock":
+        print(x + " scores")
+
+        playerOne_score = playerOne_score + 1
+        print("Your score is:", playerOne_score)
+
+    elif computer_move == "rock" and playerOne == "scissors" or "lizard":
+        print("The computer scores")
+
+        computer_score = int(computer_score) + 1
+        print("The computers score is:", computer_score)
+
+    elif computer_move == "scissors" and playerOne == "rock" or "Spock":
+        print(x + " scores")
+
+        playerOne_score = playerOne_score + 1
+        print("Your score is:", playerOne_score)
+
+    elif computer_move == "paper" and playerOne == "scissors" or "lizard":
+        print(x + " scores")
+
+        playerOne_score = playerOne_score + 1
+        print("Your score is:", playerOne_score)
+
+    elif computer_move == "scissors" and playerOne == "paper" or "lizard":
+        print("The computer scores")
+
+        computer_score = int(computer_score) + 1
+        print("The computers score is:", computer_score)
+
+    elif playerOne_score == score_limit:
+        print("Congrats! You won!")
+    elif computer_score == score_limit:
+        print("The computer won, better luck next time")
